3ce0c890d13363938e3369ad57181517ee950e78
ksasanka <35046671+ksasanka@users.noreply.github.com>
2018-02-18 19:34:20 -0800

    Add files via upload

---
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,45 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+mu=sum(X)/m;
+mu_mn=zeros(m,n);
+for i=1:m
+mu_mn(i,:)=mu;
+end;
+X_mu=X-mu_mn;
+sigma2=sum((X_mu.*X_mu))/m;
+
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
